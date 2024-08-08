4b68a32d6fa66b85a0ea7c3f54dd23ba682fd9c9
dawidtt <dawid.gorszka@gmail.com>
2024-08-08 22:47:02 +0200

    Add logic for adding book to library and add feature of removing from library

--- script.js
+++ script.js
@@ -13,13 +13,35 @@ function Book(title, author, pages, read) {
   };
 }
 
-function addBookToLibrary() {}
+function addBookToLibrary() {
+  const titleInputValue = document.querySelector("#title-input").value;
+  const authorInputValue = document.querySelector("#author-input").value;
+  const pagesInputValue = document.querySelector("#pages-input").value;
+  const readInputValue = document.querySelector("#read-input").checked;
+
+  const newBook = new Book(
+    titleInputValue,
+    authorInputValue,
+    pagesInputValue,
+    readInputValue
+  );
+  displayBook(newBook);
+}
 function displayBook(book) {
+  bookCounter += 1;
   const bookDiv = document.createElement("div");
+  bookDiv.dataset.indexInLibrary = bookCounter;
   bookDiv.classList.add("book");
 
+  const icon = document.createElement("img");
+  const iconButton = document.createElement("button");
+  icon.src = "./images/close.png";
+  icon.id = `${bookCounter}-icon`;
+  iconButton.appendChild(icon);
+  bookDiv.appendChild(iconButton);
+
   const bookTextWrap = document.createElement("div");
-  bookTextWrap.classList.add("bookTextWrap");
+  bookTextWrap.classList.add("book-text-wrap");
   bookDiv.appendChild(bookTextWrap);
 
   const title = document.createElement("p");
@@ -42,11 +64,24 @@ function displayBook(book) {
   read.textContent = `Read: ${book.read}`;
   bookTextWrap.appendChild(read);
 
+  bookDiv.addEventListener("click", removeBookFromLibrary);
   addNewBooksBtn.before(bookDiv);
 }
 
+function removeBookFromLibrary(event) {
+  bookCounter -= 1;
+
+  const currentIconId = event.target.id;
+  const currentBookNumber = currentIconId.charAt(0);
+  const selectorOfCurrentBook = `.book[data-index-in-library="${currentBookNumber}"]`;
+  const currentBook = document.querySelector(selectorOfCurrentBook);
+  currentBook.remove();
+  myLibrary.splice(currentBookNumber, 1);
+}
+let bookCounter = 0;
 const dialog = document.querySelector("#dialog");
 const closeDialog = document.querySelector("#closeDialog");
+const confirmNewBook = document.querySelector("#confirmBtn");
 const addNewBooksBtn = document.querySelector(".add-book");
 
 addNewBooksBtn.addEventListener("click", () => {
@@ -55,9 +90,20 @@ addNewBooksBtn.addEventListener("click", () => {
 closeDialog.addEventListener("click", () => {
   dialog.close();
 });
+
+confirmNewBook.addEventListener("click", preventFromSubmit, false);
+confirmNewBook.addEventListener("click", addBookToLibrary);
+
+function preventFromSubmit(event) {
+  event.preventDefault();
+  dialog.close();
+}
+
 const myLibrary = [];
 myLibrary.push(new Book("The Hobbit", "Tolkien", 295, true));
 myLibrary.push(new Book("Last Wish", "Andrzej Sapkowski", 332, false));
+myLibrary.push(new Book("www.1939.com.pl", "Marcin Ciszewski", 333, false));
+
 for (book of myLibrary) {
   displayBook(book);
 }
